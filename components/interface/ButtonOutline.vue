bc503d3bd99c001367d36cede934030e26ec15b6
Alexey Abramenko <a.abramenko@chita.ru>
2023-05-30 10:11:00 +0900

    Add. Footer

---
+++ components/interface/ButtonOutline.vue
@@ -0,0 +1,18 @@
+<template>
+	<a
+		:href="href"
+		class="block border border-dark-gray rounded-lg text-second hover:border-second hover:bg-dark-gray hover:text-white hover:bg-opacity-30"
+		><slot
+	/></a>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	href: {
+		type: String,
+		default: '',
+	},
+});
+</script>
+
+<style scoped></style>
